ebaf3ca8394d698176c075664399ad4e68a1d3f8
Jhonatan Marcelino <134547236+JhonatanMJesus@users.noreply.github.com>
2026-05-04 11:16:18 -0300

    Add files via upload

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if(txt_cidade.Text != "")
+                {
+                    Tempo? t = await DataService.GetTempo(txt_cidade.Text);
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
+
+                        dados_previsao = $"Latitude: {t.lat} \n " +
+                                        $"Longitude: {t.lon} \n " +
+                                        $"Temperatura: {t.temp}";
+                        lbl_previsao.Text = dados_previsao;
+
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                            $"type=map&location=coordinates&metricRain=mm&" +
+                            $"metricTemp=ºC&metricWind=km/h&zoom=5&overlay=wind&" +
+                            $"product=ecmwf&level=surface&" +
+                            $"lat={t.lat.ToString().Replace(",", ".")}&" +
+                            $"lon={t.lon.ToString().Replace(",", ".")}";
+
+                        wv_mapa.Source = mapa;
+                    }
+                }
+                else
+                {
+                    throw new Exception("Informe a cidade.");
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlertAsync("Ops", ex.Message, "Ok");
+            }
+        }
+    }
+}
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json.Linq;
+using MauiAppTempoAgora.Models;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo> GetTempo(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "a81608e330afee8ba11be210531bce7f";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime tempo = new();
+                    DateTime sunrise = tempo.AddSeconds((double)rascunho["sys"]["sunrise"]);
+                    DateTime sunset = tempo.AddSeconds((double)rascunho["sys"]["sunset"]);
+
+                    t = new()
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["main"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+
+                        temp = (double)rascunho["main"]["temp"],
+                        feels_like = (double)rascunho["main"]["feels_like"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise,
+                        sunset = sunset,
+                        timezone = (int)rascunho["timezone"],
+                        icon = (string)rascunho["weather"][0]["icon"]
+                    };
+                }
+            }
+
+            return t;
+        }
+    }
+}
